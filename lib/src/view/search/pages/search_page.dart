5c4fc3e82acb73334e223477bf47fbf5261b1f6e
Rifat Hossain <88751768+rifathossain82@users.noreply.github.com>
2023-09-25 19:12:57 +0600

    Added search ui (#7)
    NW

---
+++ lib/src/view/search/pages/search_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:wallpaper_app/src/view/search/widgets/search_field_widget.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final searchTextController = TextEditingController();
+  bool hasText = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    searchTextController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        titleSpacing: 10,
+        title: SearchFieldWidget(
+          controller: searchTextController,
+          hasText: hasText,
+          onChanged: (value){
+            setState(() {
+              hasText = true;
+            });
+          },
+          onClose: (){
+            setState(() {
+              hasText = false;
+              searchTextController.clear();
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/view/search/widgets/search_field_widget.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:wallpaper_app/src/core/utils/color.dart';
+
+class SearchFieldWidget extends StatelessWidget {
+  final TextEditingController controller;
+  final bool hasText;
+  final ValueChanged onChanged;
+  final VoidCallback onClose;
+
+  const SearchFieldWidget({
+    Key? key,
+    required this.controller,
+    required this.hasText,
+    required this.onChanged,
+    required this.onClose,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 45,
+      child: TextField(
+        controller: controller,
+        maxLines: 1,
+        textAlign: TextAlign.start,
+        keyboardType: TextInputType.text,
+        textInputAction: TextInputAction.search,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(
+            vertical: 0,
+            horizontal: 8,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide.none,
+          ),
+          filled: true,
+          fillColor: kWhite.withOpacity(0.1),
+          hintText: 'Search here',
+          isDense: true,
+          suffixIcon: hasText
+              ? IconButton(
+                  icon: const Icon(
+                    Icons.close,
+                    size: 20,
+                  ),
+                  onPressed: onClose,
+                )
+              : const Icon(
+                  Icons.search,
+                  size: 20,
+                ),
+        ),
+      ),
+    );
+  }
+}
