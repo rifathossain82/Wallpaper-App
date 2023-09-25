f63b75eca5d67d50cfb662c45a7ca44097f99642
rifathossain82 <officialrifat82@gmail.com>
2023-09-25 17:18:32 +0600

    premium ui added

---
+++ lib/src/view/premium/pages/premium_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:wallpaper_app/src/core/widgets/wallpapers_grid_view_widget.dart';
+
+class PremiumPage extends StatelessWidget {
+  const PremiumPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const WallpapersGridViewWidget();
+  }
+}
